55cc9dffbcab59deb68bedaf59c46b845c0b7ecf
EddyGharbi <eddygharbiema@gmail.com>
2021-01-19 09:26:12 -0800

    wip

--- src/windows/window-rendering-win32.cpp
+++ src/windows/window-rendering-win32.cpp
@@ -16,33 +16,87 @@ ID3D11Device* device_ptr                       = NULL;
 ID3D11DeviceContext* device_context_ptr        = NULL;
 IDXGISwapChain* swap_chain_ptr                 = NULL;
 ID3D11RenderTargetView* render_target_view_ptr = NULL; 
+ID3D11ShaderResourceView* shaderRes;
 
 LRESULT CALLBACK DisplayWndProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
 {
 	if (shouldRender) {
-		/* clear the back buffer to cornflower blue for the new frame */
-		float background_colour[4] = {
-		0x64 / 255.0f, 0x95 / 255.0f, 0xED / 255.0f, 1.0f };
-		device_context_ptr->ClearRenderTargetView(
-			render_target_view_ptr,
-			background_colour
+		HRESULT hr;
+
+		ID3D11Texture2D* framebuffer;
+		hr = device_ptr->OpenSharedResource((HANDLE)(uintptr_t)g_sharedHandle,
+							__uuidof(ID3D11Texture2D),
+							(void **)&framebuffer);
+		if (FAILED(hr))
+			return NULL;
+
+		IDXGIKeyedMutex *km;
+		hr = framebuffer->QueryInterface(
+			IID_IDXGIKeyedMutex,
+			(void**)&km
 		);
 
-		RECT winRect;
-		GetClientRect( nwr_window, &winRect );
+		framebuffer->SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM);
+
+		km->AcquireSync(0, 5);
+		
+		D3D11_TEXTURE2D_DESC desc;
+		framebuffer->GetDesc(&desc);
+		
+		ID3D11Texture2D *output_tex;
+		hr = device_ptr->CreateTexture2D(&desc, NULL, &output_tex);
+		if (FAILED(hr)) return false;
+
+		device_context_ptr->CopyResource(
+			(ID3D11Resource *)output_tex,
+			(ID3D11Resource *)framebuffer);
+
+		km->ReleaseSync(0);
+
+
+		D3D11_TEXTURE2D_DESC desc2;
+		output_tex->GetDesc(&desc2);
+
+		D3D11_SHADER_RESOURCE_VIEW_DESC resourceDesc = {};
+		resourceDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		resourceDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		resourceDesc.Texture2D.MipLevels = 1;
+		hr = device_ptr->CreateShaderResourceView(output_tex, &resourceDesc, &shaderRes);
+		if (FAILED(hr))
+			return NULL;
+
+		ID3D11SamplerState *m_TexSamplerState;
+		D3D11_SAMPLER_DESC sampDesc;
+		ZeroMemory( &sampDesc, sizeof(sampDesc) );
+		sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+		sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+		sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+
+
+		hr = device_ptr->CreateSamplerState( &sampDesc, &m_TexSamplerState );
+		if (FAILED(hr))
+			return NULL;
+
+		// Set the sampler state in the pixel shader.
+		device_context_ptr->PSSetSamplers(0, 1, &m_TexSamplerState);
+		// device_context_ptr->PSSetShaderResources(0, 1, &shaderRes);
+
+		// device_context_ptr->OMSetRenderTargets( 1, &render_target_view_ptr, NULL );
+
+
 		D3D11_VIEWPORT viewport = {
 			0.0f,
 			0.0f,
-			( FLOAT )( winRect.right - winRect.left ),
-			( FLOAT )( winRect.bottom - winRect.top ),
+			1920,
+			1080,
 			0.0f,
 			1.0f
 		};
 		device_context_ptr->RSSetViewports( 1, &viewport );
 
-		device_context_ptr->OMSetRenderTargets( 1, &render_target_view_ptr, NULL );
-
 		swap_chain_ptr->Present( 0, 0 );
+		
 	}
 
 
@@ -124,9 +178,7 @@ void createWindow(std::string name, void **handle) {
 		return;
 
 	DXGI_SWAP_CHAIN_DESC swap_chain_descr               = { 0 };
-	swap_chain_descr.BufferDesc.RefreshRate.Numerator   = 0;
-	swap_chain_descr.BufferDesc.RefreshRate.Denominator = 1; 
-	swap_chain_descr.BufferDesc.Format  = DXGI_FORMAT_B8G8R8A8_UNORM; 
+	swap_chain_descr.BufferDesc.Format  = DXGI_FORMAT_R8G8B8A8_UNORM; 
 	swap_chain_descr.SampleDesc.Count   = 1;                               
 	swap_chain_descr.SampleDesc.Quality = 0;                               
 	swap_chain_descr.BufferUsage        = DXGI_USAGE_RENDER_TARGET_OUTPUT;
@@ -157,27 +209,6 @@ void createWindow(std::string name, void **handle) {
 	
 	if (FAILED(hr))
 		return;
-
-	ID3D11Texture2D* framebuffer;
-	hr = swap_chain_ptr->GetBuffer(
-		0,
-		__uuidof( ID3D11Texture2D ),
-		(void**)&framebuffer
-	);
-
-	if (FAILED(hr))
-		return;
-
-	hr = device_ptr->CreateRenderTargetView(
-		framebuffer,
-		0,
-		&render_target_view_ptr
-	);
-
-	if (FAILED(hr))
-		return;
-
-	framebuffer->Release();
 
     return;
 }
