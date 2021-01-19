d594ab4f6ff0cd6067a04955810c59c41f34c16d
EddyGharbi <eddygharbiema@gmail.com>
2021-01-19 15:25:41 -0800

    minor cleanup

--- src/windows/window-rendering-win32.cpp
+++ src/windows/window-rendering-win32.cpp
@@ -57,9 +57,7 @@ struct cbPerObject
 
 cbPerObject cbPerObj;
 
-///////////////**************new**************////////////////////
-//Vertex Structure and Vertex Layout (Input Layout)//
-struct Vertex	//Overloaded Vertex Structure
+struct Vertex
 {
 	Vertex(){}
 	Vertex(float x, float y, float z,
@@ -79,16 +77,11 @@ UINT numElements = ARRAYSIZE(layout);
 
 void UpdateScene()
 {
-	//Keep the cubes rotating
-	// rot += .0005f;
-	// if(rot > 6.26f)
-	// 	rot = 0.0f;
-
 	//Reset cube1World
 	cube1World = DirectX::XMMatrixIdentity();
 
 	//Define cube1's world space matrix
-	DirectX::XMVECTOR rotaxis = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	DirectX::XMVECTOR rotaxis = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 	Rotation = DirectX::XMMatrixRotationAxis( rotaxis, rot);
 	Translation = DirectX::XMMatrixTranslation( 0.0f, 0.0f, 4.0f );
 
@@ -121,25 +114,12 @@ void DrawScene() {
 		cbPerObj.WVP = XMMatrixTranspose(WVP);	
 		device_context_ptr->UpdateSubresource( cbPerObjectBuffer, 0, NULL, &cbPerObj, 0, 0 );
 		device_context_ptr->VSSetConstantBuffers( 0, 1, &cbPerObjectBuffer );
-		///////////////**************new**************////////////////////
+		
 		device_context_ptr->PSSetShaderResources( 0, 1, &shaderRes );
 		device_context_ptr->PSSetSamplers( 0, 1, &CubesTexSamplerState );
-		///////////////**************new**************////////////////////
 
 		//Draw the first cube
 		device_context_ptr->DrawIndexed( 36, 0, 0 );
-
-		// WVP = cube2World * camView * camProjection;
-		// cbPerObj.WVP = XMMatrixTranspose(WVP);	
-		// device_context_ptr->UpdateSubresource( cbPerObjectBuffer, 0, NULL, &cbPerObj, 0, 0 );
-		// device_context_ptr->VSSetConstantBuffers( 0, 1, &cbPerObjectBuffer );
-		// ///////////////**************new**************////////////////////
-		// device_context_ptr->PSSetShaderResources( 0, 1, &shaderRes );
-		// device_context_ptr->PSSetSamplers( 0, 1, &CubesTexSamplerState );
-		///////////////**************new**************////////////////////
-
-		//Draw the second cube
-		// device_context_ptr->DrawIndexed( 36, 0, 0 );
 
 		//Present the backbuffer to the screen
 		swap_chain_ptr->Present(0, 0);
@@ -149,126 +129,7 @@ LRESULT CALLBACK DisplayWndProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wPar
 {
 	if (shouldRender) {
 		UpdateScene();
-		DrawScene();
-		// HRESULT hr;
-
-		// ID3D11Texture2D* framebuffer;
-		// hr = device_ptr->OpenSharedResource((HANDLE)(uintptr_t)g_sharedHandle,
-		// 					__uuidof(ID3D11Texture2D),
-		// 					(void **)&framebuffer);
-		// if (FAILED(hr))
-		// 	return NULL;
-
-		// IDXGIKeyedMutex *km;
-		// hr = framebuffer->QueryInterface(
-		// 	IID_IDXGIKeyedMutex,
-		// 	(void**)&km
-		// );
-
-		// framebuffer->SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM);
-
-		// km->AcquireSync(0, 5);
-		
-		// D3D11_TEXTURE2D_DESC desc;
-		// framebuffer->GetDesc(&desc);
-		
-		// ID3D11Texture2D *output_tex;
-		// hr = device_ptr->CreateTexture2D(&desc, NULL, &output_tex);
-		// if (FAILED(hr)) return false;
-
-		// device_context_ptr->CopyResource(
-		// 	(ID3D11Resource *)output_tex,
-		// 	(ID3D11Resource *)framebuffer);
-
-		// km->ReleaseSync(0);
-
-
-		// D3D11_TEXTURE2D_DESC desc2;
-		// output_tex->GetDesc(&desc2);
-
-		// D3D11_SHADER_RESOURCE_VIEW_DESC resourceDesc = {};
-		// resourceDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		// resourceDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-		// resourceDesc.Texture2D.MipLevels = 1;
-		// hr = device_ptr->CreateShaderResourceView(output_tex, &resourceDesc, &shaderRes);
-		// if (FAILED(hr))
-		// 	return NULL;
-
-		// ID3D11SamplerState *m_TexSamplerState;
-		// D3D11_SAMPLER_DESC sampDesc;
-		// ZeroMemory( &sampDesc, sizeof(sampDesc) );
-		// sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-		// sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-		// sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-		// sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-
-
-		// hr = device_ptr->CreateSamplerState( &sampDesc, &m_TexSamplerState );
-		// if (FAILED(hr))
-		// 	return NULL;
-
-		// // Set the sampler state in the pixel shader.
-		// device_context_ptr->PSSetSamplers(0, 1, &m_TexSamplerState);
-		// // device_context_ptr->PSSetShaderResources(0, 1, &shaderRes);
-
-		// // device_context_ptr->OMSetRenderTargets( 1, &render_target_view_ptr, NULL );
-
-
-		// D3D11_VIEWPORT viewport = {
-		// 	0.0f,
-		// 	0.0f,
-		// 	1920,
-		// 	1080,
-		// 	0.0f,
-		// 	1.0f
-		// };
-		// device_context_ptr->RSSetViewports( 1, &viewport );
-
-		// swap_chain_ptr->Present( 0, 0 );
-
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-
-		// //Clear our backbuffer
-		// float bgColor[4] = {(0.0f, 0.0f, 0.0f, 0.0f)};
-		// device_context_ptr->ClearRenderTargetView(render_target_view_ptr, bgColor);
-
-		// //Refresh the Depth/Stencil view
-		// device_context_ptr->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
-
-		// //Set the WVP matrix and send it to the constant buffer in effect file
-		// WVP = cube1World * camView * camProjection;
-		// cbPerObj.WVP = XMMatrixTranspose(WVP);	
-		// device_context_ptr->UpdateSubresource( cbPerObjectBuffer, 0, NULL, &cbPerObj, 0, 0 );
-		// device_context_ptr->VSSetConstantBuffers( 0, 1, &cbPerObjectBuffer );
-		// ///////////////**************new**************////////////////////
-		// device_context_ptr->PSSetShaderResources( 0, 1, &shaderRes );
-		// device_context_ptr->PSSetSamplers( 0, 1, &CubesTexSamplerState );
-		// ///////////////**************new**************////////////////////
-
-		// //Draw the first cube
-		// device_context_ptr->DrawIndexed( 36, 0, 0 );
-
-		// WVP = cube2World * camView * camProjection;
-		// cbPerObj.WVP = XMMatrixTranspose(WVP);	
-		// device_context_ptr->UpdateSubresource( cbPerObjectBuffer, 0, NULL, &cbPerObj, 0, 0 );
-		// device_context_ptr->VSSetConstantBuffers( 0, 1, &cbPerObjectBuffer );
-		// ///////////////**************new**************////////////////////
-		// device_context_ptr->PSSetShaderResources( 0, 1, &shaderRes );
-		// device_context_ptr->PSSetSamplers( 0, 1, &CubesTexSamplerState );
-		// ///////////////**************new**************////////////////////
-
-		// //Draw the second cube
-		// device_context_ptr->DrawIndexed( 36, 0, 0 );
-
-		// //Present the backbuffer to the screen
-		// swap_chain_ptr->Present(0, 0);
-		
+		DrawScene();	
 	}
 
 
@@ -348,15 +209,6 @@ void createWindow(std::string name, void **handle) {
 	HMODULE dxgi = load_system_library("dxgi.dll");
 	if (!dxgi)
 		return;
-
-	// DXGI_SWAP_CHAIN_DESC swap_chain_descr               = { 0 };
-	// swap_chain_descr.BufferDesc.Format  = DXGI_FORMAT_R8G8B8A8_UNORM; 
-	// swap_chain_descr.SampleDesc.Count   = 1;                               
-	// swap_chain_descr.SampleDesc.Quality = 0;                               
-	// swap_chain_descr.BufferUsage        = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	// swap_chain_descr.BufferCount        = 1;                               
-	// swap_chain_descr.OutputWindow       = nwr_window;                
-	// swap_chain_descr.Windowed           = true;
 
 	//Describe our SwapChain Buffer
 	DXGI_MODE_DESC bufferDesc;
@@ -455,10 +307,6 @@ void connectSharedMemory(std::string name, uint32_t sharedHandle) {
 
 	//Set our Render Target
 	device_context_ptr->OMSetRenderTargets( 1, &render_target_view_ptr, depthStencilView );
-
-	// Init Scene
-	// hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "VS", "vs_4_0", 0, 0, 0, &VS_Buffer, 0, 0);
-	// hr = D3DX11CompileFromFile(L"Effects.fx", 0, 0, "PS", "ps_4_0", 0, 0, 0, &PS_Buffer, 0, 0);
 
     hr = D3DCompileFromFile(
       L"Effects.fx",
@@ -648,10 +496,6 @@ void connectSharedMemory(std::string name, uint32_t sharedHandle) {
 	//Set the Projection matrix
 	camProjection = DirectX::XMMatrixPerspectiveFovLH( 0.4f*3.14f, 1920/1080, 1.0f, 1000.0f);
 
-	///////////////**************new**************////////////////////
-	// hr = D3DX11CreateShaderResourceViewFromFile( d3d11Device, L"braynzar.jpg",
-	// 	NULL, NULL, &CubesTexture, NULL );
-
 		
 	D3D11_SHADER_RESOURCE_VIEW_DESC resourceDesc = {};
 	resourceDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
@@ -682,7 +526,6 @@ void connectSharedMemory(std::string name, uint32_t sharedHandle) {
     
 	//Create the Sample State
 	hr = device_ptr->CreateSamplerState( &sampDesc, &CubesTexSamplerState );
-	///////////////**************new**************////////////////////
 
 	shouldRender = true;
 }
