dce8b7570cdc9278d86fc5d86fbc3319a4c0052c
EddyGharbi <eddygharbiema@gmail.com>
2022-10-11 09:50:17 -0700

    expose new API to render the display at the bottom (#6)

--- src/window-osx-obj-c-int.h
+++ src/window-osx-obj-c-int.h
@@ -30,7 +30,7 @@ public:
     ~WindowObjCInt(void);
 
     void init(void);
-    void createWindow(std::string name, void **handle);
+    void createWindow(std::string name, void **handle, bool renderAtBottom);
     void destroyWindow(std::string name);
     void connectIOSurfaceJS(std::string name, uint32_t surfaceID);
     void destroyIOSurface(std::string name);
